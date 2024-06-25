4ca30ecd6e19784f32631a5d14c94715c35a692b
gbatt <gbatt@amazon.com>
2024-06-25 16:29:21 +0100

    BUG: empty systemPrompt return

--- resources/ui/src/components/QAManager.jsx
+++ resources/ui/src/components/QAManager.jsx
@@ -52,12 +52,11 @@ export function QAManager({ inferenceURL, creds, region, appConfig }) {
           isModified: true,
           values: parsedPrompt
         }
-
       }
-      return {
-        isModified: false,
-        values:[]
-      }
+    }
+    return {
+      isModified: false,
+      values:[]
     }
   });
 
